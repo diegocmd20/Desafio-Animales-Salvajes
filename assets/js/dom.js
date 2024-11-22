79d406123a9fc22aa161390d68aa16d2f5794633
diegocmd20 Home <diegocmd20@gmail.com>
2024-11-21 23:23:42 -0300

    Animal card upgraded

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -2,8 +2,7 @@ export function agregarAnimalATabla(animal) {
     const animalesDiv = document.getElementById("Animales");
 
     const card = document.createElement("div");
-    card.classList.add("card", "bg-dark", "text-white", "m-2");
-    card.style.width = "200px";
+    card.classList.add("card", "bg-dark", "text-white", "h-25", "w-25");
 
     const img = document.createElement("img");
     img.classList.add("card-img-top");
@@ -19,9 +18,19 @@ export function agregarAnimalATabla(animal) {
     title.classList.add("card-title");
     title.textContent = animal.nombre;
 
+    const footer = document.createElement("div");
+    footer.classList.add("card-footer", "bg-secondary", "d-flex", "align-items-center", "justify-content-center", "py-2", "px-2");
+    
+    const imgFooter = document.createElement("img");
+    imgFooter.src = "assets/images/../imgs/audio.svg";
+    imgFooter.style.height = ("auto");
+    imgFooter.style.width = ("1.5rem");
+
     body.appendChild(title);
     card.appendChild(img);
     card.appendChild(body);
+    card.appendChild(footer);
+    footer.appendChild(imgFooter);
 
     animalesDiv.appendChild(card);
 }
